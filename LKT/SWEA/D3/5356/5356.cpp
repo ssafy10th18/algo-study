4f33c9cda090a20eac47597b58630846b5c660d2
lkt9899 <lkt9899@naver.com>
2023-08-01 14:21:30 +0900

    [SWEA] 의석이의 세로로 말해요 / D3 / 10분

--- LKT/SWEA/D3/5356/5356.cpp
+++ LKT/SWEA/D3/5356/5356.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
+#include <string>
 #define fio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
 using namespace std;
 
-void init() {}
+int MAX;
+string arr[5];
 
-void input() {}
+void init() { MAX = 0; }
 
-void sol() {}
+void input() {
+    for (int i = 0; i < 5; i++) {
+        cin >> arr[i];
+        MAX = MAX > arr[i].length() ? MAX : arr[i].length();
+    }
+}
+
+void sol() {
+    for (int j = 0; j < MAX; j++) {
+        for (int i = 0; i < 5; i++) {
+            if (j >= arr[i].length()) continue;
+            cout << arr[i][j];
+        }
+    }
+}
 
 void run() {
     init();
     input();
     sol();
 }
+
 int main(int argc, char** argv) {
     fio;
     int T, test_case;
@@ -21,8 +38,9 @@ int main(int argc, char** argv) {
     freopen("input.txt", "r", stdin);
     cin >> T;
     for (test_case = 1; test_case <= T; ++test_case) {
+        cout << "#" << test_case << " ";
         run();
-        cout << "#" << test_case << " " << ans << "\n";
+        cout << "\n";
     }
     return 0;
 }
