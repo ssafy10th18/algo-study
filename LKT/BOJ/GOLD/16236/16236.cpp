2c60aab086d42eb45b150f7ab95cd1de8537a100
lkt9899 <lkt9899@naver.com>
2023-08-03 16:47:21 +0900

    [BOJ] 아기 상어 / 골드3 / 2시간

--- LKT/BOJ/GOLD/16236/16236.cpp
+++ LKT/BOJ/GOLD/16236/16236.cpp
@@ -1,6 +1,8 @@
+#include <algorithm>
 #include <cstring>
 #include <iostream>
 #include <queue>
+#include <vector>
 #define fio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #define pii pair<int, int>
 
@@ -9,6 +11,16 @@ using namespace std;
 struct Axis {
     int x, y, t;
 };
+
+bool compare(Axis a, Axis b) {
+    if (a.t == b.t) {
+        if (a.x == b.x) {
+            return a.y < b.y;
+        }
+        return a.x < b.x;
+    }
+    return a.t < b.t;
+}
 
 /* 0    : 빈칸
  * 1~6  : 물고기
@@ -21,6 +33,7 @@ int visited[MAX][MAX];
 int di[4] = {-1, 0, 0, 1};
 int dj[4] = {0, -1, 1, 0};
 Axis start;
+vector<Axis> toEat;
 
 void init() {
     age = 2;
@@ -46,34 +59,42 @@ void find(Axis cur, int cnt) {
         age++;
     }
 
+    toEat.clear();
     memset(visited, 0, sizeof(visited));
 
     queue<Axis> q;
     q.push(cur);
     visited[cur.x][cur.y] = 1;
-
     while (!q.empty()) {
         Axis c = q.front();
         q.pop();
+
+        if (!toEat.empty() && c.t > toEat[0].t) break;
 
         for (int i = 0; i < 4; i++) {
             int ni = c.x + di[i];
             int nj = c.y + dj[i];
 
             if (ni < 0 || ni >= N || nj < 0 || nj >= N || visited[ni][nj] != 0) continue;
-
             if (map[ni][nj] > age) continue;
 
             visited[ni][nj] = 1;
             if (map[ni][nj] != 0 && map[ni][nj] < age) {
-                map[ni][nj] = 0;
-                cnt++;
-                ans += c.t + 1;
-
-                find({ni, nj, 0}, cnt);
+                toEat.push_back({ni, nj, c.t + 1});
+            } else {
+                q.push({ni, nj, c.t + 1});
             }
-            q.push({ni, nj, c.t + 1});
         }
+    }
+
+    if (!toEat.empty()) {
+        sort(toEat.begin(), toEat.end(), compare);
+
+        map[toEat[0].x][toEat[0].y] = 0;
+        cnt++;
+        ans += toEat[0].t;
+        toEat[0].t = 0;
+        find(toEat[0], cnt);
     }
 }
 
