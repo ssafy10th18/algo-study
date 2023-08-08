ecc92cc1d3a9842bd13773a9363b3308c8e362d5
lkt9899 <80976609+lkt9899@users.noreply.github.com>
2023-08-09 00:51:45 +0900

    [BOJ] 요세푸스 문제 2 / 플레3 / 5시간

--- LKT/BOJ/PLATINUM/1168/1168.cpp
+++ LKT/BOJ/PLATINUM/1168/1168.cpp
@@ -11,31 +11,33 @@ int init(int node, int start, int end) {
         return seg[node] = 1;
     }
 
-    int mid = (start + end) / 2;
-    return seg[node] = init(2 * node, start, mid) + init(2 * node + 1, mid + 1, end);
+    int mid = (start + end) >> 1;
+    return seg[node] = init(node << 1, start, mid) + init((node << 1) + 1, mid + 1, end);
 }
 
 int query(int node, int start, int end, int order) {
-    int mid = (start + end) / 2;
     if (start == end) return start;
 
-    if (order <= seg[2 * node]) {
-        return query(2 * node, start, mid, order);
+    int mid = (start + end) >> 1;
+    if (order <= seg[node << 1]) {
+        return query(node << 1, start, mid, order);
     } else {
-        return query(2 * node + 1, mid + 1, end, order - seg[2 * node]);
+        return query((node << 1) + 1, mid + 1, end, order - seg[node << 1]);
     }
 }
 
 int update(int node, int start, int end, int del) {
     seg[node]--;
-    if (start == end) return 0;
+    if (start == end)
+        return 0;
+    else {
+        int mid = (start + end) / 2;
 
-    int mid = (start + end) / 2;
-
-    if (del <= mid) {
-        return update(2 * node, start, mid, del);
-    } else {
-        return update(2 * node + 1, mid + 1, end, del);
+        if (del <= mid) {
+            return update(node << 1, start, mid, del);
+        } else {
+            return update((node << 1) + 1, mid + 1, end, del);
+        }
     }
 }
 
@@ -43,28 +45,20 @@ void input() { cin >> N >> M; }
 
 void sol() {
     init(1, 1, N);
-    int c = 1;
-    for (int i = 0; i <= 4; i++) {
-        while (c < (1 << i)) {
-            cout << seg[c] << " ";
-            c++;
-        }
-        cout << "\n";
-    }
 
-    cout << "<";
     int index = 1;
+    cout << "<";
     for (int i = 0; i < N; i++) {
         int size = N - i;
         index += M - 1;
 
-        if (index == size)
+        if (index % size == 0)
             index = size;
         else if (index > size)
             index %= size;
+
         int order = query(1, 1, N, index);
         update(1, 1, N, order);
-
         if (i == N - 1) {
             cout << order;
         } else {
