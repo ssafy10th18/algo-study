ef03140fa5111d27f7212f0086f9cd69b89801d1
jinju9553 <jinju9553@gmail.com>
2023-09-02 18:32:17 +0900

    Feat: 6주차 문제풀이

--- jinju/src/swea/Ad/swea1767/Solution.java
+++ jinju/src/swea/Ad/swea1767/Solution.java
@@ -1,22 +1,22 @@
 package swea.Ad.swea1767;
 
-import java.awt.Point;
 import java.io.*;
 import java.util.*;
 
 public class Solution {
-
-	private static final int UP = 0;
-	private static final int DOWN = 1;
-	private static final int RIGHT = 2;
-	private static final int LEFT = 3;
-		
-	private static Point[] deltas = {new Point(0, 1), new Point(0, -1), 
-			new Point(1, 0), new Point(-1, 0)};
+	
+	private static int N;
+	private static int answer;
+	private static int maxConnections = 0;
 	
 	private static int[] directions;
 	private static int[][] board;
+	private static boolean[][] visited;
+	
 	private static List<Core> cores;
+	
+	private static int[] dr = {-1, 1, 0, 0};
+	private static int[] dc = {0, 0, 1, -1};
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,10 +24,11 @@ public class Solution {
 
 		StringBuilder sb = new StringBuilder();
 		for (int test_case = 1; test_case <= T; test_case++) {
-			int N = Integer.parseInt(br.readLine());
+			N = Integer.parseInt(br.readLine());
+			answer = Integer.MAX_VALUE;
 			
-			directions = new int[N];
 			board = new int[N][N];
+			visited = new boolean[N][N];
 			cores = new ArrayList<>();
 			
 			StringTokenizer st;
@@ -36,46 +37,107 @@ public class Solution {
 				for(int j = 0; j < N; j++) {
 					board[i][j] = Integer.parseInt(st.nextToken());
 					
-					if(board[i][j] == 1) {
-						cores.add(new Core(i, j, 0, 0));
+					if(board[i][j] == 1 && !isBorder(i, j)) {
+						cores.add(new Core(i, j));
 					}
 				}
 			}
 			
+			directions = new int[cores.size()];
+			
 			permutation(0);
 			
-			sb.append("#");
+			sb.append("#").append(test_case).append(" ").append(answer).append("\n");
 		}
 		
 		System.out.println(sb);
 	}
-	
-	public static void DFS(int startR, int startC, int dir) {
-		
+
+	private static boolean isBorder(int r, int c) {
+		return (r - 1 < 0 || c - 1 < 0 || r + 1 == N || c + 1 == N);
 	}
 
 	private static void permutation(int depth) {
 		if(depth == directions.length) {
-			//지금까지 만들어진 방향에 따라 전선을 연결할 수 있는지 확인
+			//각 코어가 지금까지 만들어진 방향을 따라 전선을 연결할 수 있는지 확인	
+			checkConnection();
+			return;
 		}
 		
+		Core core = cores.get(depth);
 		for(int i = 0; i < 4; i++) {
-			directions[depth] = i; //depth번 코어의 방향 결정 (중복 순열)
+			//나아갈 수 없는 방향이라면 아예 뽑지 않는다.
+			int nr = core.row + dr[i];
+			int nc = core.col + dc[i];
+			
+			if(!isValid(nr, nc)) {
+				continue;
+			}
+			
+			directions[depth] = i; 
 			permutation(depth + 1);
 		}
+	}
+
+	private static void checkConnection() {
+		visited = new boolean[N][N];
+		
+		int connections = 0;
+		int result = 0;
+		for(int i = 0; i < directions.length; i++) {
+			Core core = cores.get(i);
+
+			int nr = 0;
+			int nc = 0;
+			
+			int currLength = 0;
+			boolean isPossible = true;
+			for(int k = 1; k < N; k++) {
+				nr = core.row + dr[directions[i]] * k;
+				nc = core.col + dc[directions[i]] * k;
+				
+				if(!isValid(nr, nc)) {
+					break; //탐색 종료
+				}
+				
+				//전선이 교차되는 경우 + 다른 프로세서를 지나는 경우
+				if(visited[nr][nc] || board[nr][nc] == 1) {
+					isPossible = false; //이 전선은 합산하지 않는다.
+					break; //탐색 종료
+				}
+				
+				visited[nr][nc] = true;
+				currLength += 1;
+			}
+			
+			//벽에 다다를 수 있다면(전선 교차 X,다른 프로세서 지나지 않음) 합산
+			if(isPossible) {
+				connections++; //연결된 프로세서의 수 증가
+				result += currLength;
+			}
+		}
+		
+		if(result > 0) {
+			if(maxConnections < connections) {
+				maxConnections = connections;
+				answer = result;
+			} else if(maxConnections == connections) {
+				answer = Math.min(result, answer);
+			}
+		}
+	}
+
+	private static boolean isValid(int r, int c) {
+		return (r > -1 && c > -1 && r < N && c < N);
 	}
 }
 
 class Core {
 	int row;
 	int col;
-	int dir;
-	int length;
 	
-	public Core(int row, int col, int dir, int length) {
+	public Core(int row, int col) {
 		this.row = row;
 		this.col = col;
-		this.dir = dir;
-		this.length = length;
 	}
 }
