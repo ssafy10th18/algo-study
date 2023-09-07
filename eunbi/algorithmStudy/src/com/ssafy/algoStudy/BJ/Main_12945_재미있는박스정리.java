bdbdc48751d7176e593747d7effd97f2a139c2ce
muuuumbi <dnpfkdlvm123@gmail.com>
2023-09-07 17:15:46 +0900

    campus

--- eunbi/algorithmStudy/src/com/ssafy/algoStudy/BJ/Main_12945_재미있는박스정리.java
+++ eunbi/algorithmStudy/src/com/ssafy/algoStudy/BJ/Main_12945_재미있는박스정리.java
@@ -23,8 +23,6 @@ public class Main_12945_재미있는박스정리 {
 		 * [입력]
 		 * 1. 박스의 개수 N (1 <= N <= 500,000)
 		 * 2~N. 박스들의 크기 V (1 <= V <= 100,000)
-		 */
-		init();
 		/**
 		 * [출력]
 		 * 최적의 경우
@@ -32,65 +30,5 @@ public class Main_12945_재미있는박스정리 {
 		result = N - pair*2 + pair;
 		System.out.println(result);
 	}
-	
-	static void init() throws IOException{
-		N = Integer.parseInt(br.readLine());
-		pair = 0;
-		for(int n = 0; n < N; n++) {
-			int size = Integer.parseInt(br.readLine());
-			boxes[size] += 1;
-		}
-		
-		search(100000, 50000);
-	}
-	
-	static void search(int bigP, int smallP) {
-		if(bigP < 1 || smallP < 1)
-			return;
-		
-		//큰 상자가 존재하면
-		if(boxes[bigP] != 0) {
-			//작은 상자가 존재하면
-			if(boxes[smallP] != 0) {
-				//사이즈 비교
-				//큰 상자 안에 넣을 수 있으면
-				if(compareSize(bigP, smallP)) {
-					//개수 줄여주기
-					boxes[bigP] -= 1;
-					boxes[smallP] -= 1;
-					//쌍 개수 늘려주기
-					pair++;
-					//값 다시 넣어주기
-					search(bigP, smallP);
-					return;
-				}
-				else {
-					search()
-				}
-			}
-			//작은 상자 없으면
-			else {
-				search(bigP, smallP - 1);
-				return;
-			}
-		}
-		//큰 상자 없으면
-		else {
-			//작은 상자 존재하면
-			if(boxes[smallP] != 0) {
-				search(bigP - 1, smallP);
-				return;
-			}
-			//작은 상자 없으면
-			else {
-				search(bigP - 1, smallP - 1);
-				return;
-			}
-		}
-		
-	}
-	
-	static boolean compareSize(int big, int small) {
-		return big >= small*2;
-	}
+
 }
