5c03ae858e4037dc306f3a450824f72c67db169a
jinju9553 <jinju9553@gmail.com>
2023-09-11 16:30:27 +0900

    Feat : 7주차 문제풀이 보충

--- jinju/src/boj/gold/boj2613/Main.java
+++ jinju/src/boj/gold/boj2613/Main.java
@@ -9,8 +9,10 @@ public class Main {
 
 	private static int N;
 	private static int M;
+
+	private static int[] beads;
 	
-	private static int[] DP;
+	private static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,9 +22,8 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		int[] beads = new int[N];
-		DP = new int[N];
-		
+		beads = new int[N];
+
 		int start = 0;
 		int end = 0;
 		st = new StringTokenizer(br.readLine(), " ");
@@ -32,17 +33,13 @@ public class Main {
 			start = Math.max(beads[i], start);
 			end += beads[i];
 		}
-		
-		DP[0] = beads[0];
-		for(int i = 1; i < N; i++) {
-			DP[i] = DP[i - 1] + beads[i];
-		}
 
 		int answer = Integer.MAX_VALUE;
 		
-		//이분탐색
+		int mid = 0;
+		//parametric search
 		while(start <= end) {
-			int mid = (start + end) / 2;
+			mid = (start + end) / 2;
 			
 			if(isValidGroup(mid)) { //그룹의 수가 적다면
 				end = mid - 1; //최댓값을 더 작게 잡는다.
@@ -52,25 +49,51 @@ public class Main {
 			}
 		}
 		
-		System.out.println(answer);
+		sb.append(answer).append("\n"); 
+		
+		printGroup(answer, M);
+		
+		System.out.println(sb);
+	}
+
+	private static void printGroup(int mid, int target) {
+		int sum = 0;
+		int count = 0;
+		
+		for(int i = 0; i < beads.length; i++) {
+			sum += beads[i];
+			
+			if(sum > mid) {
+				sum = beads[i]; //새로운 구간부터 다시 부분합을 구한다.
+				//한 그룹을 모두 묶었으므로 append 해준다.
+				sb.append(count).append(" ");
+				
+				target -= 1; //앞으로 뽑을 그룹 수 감소
+				count = 0;
+			}
+			count += 1;
+			
+			//앞으로 구할 그룹의 수와 잔여 구슬의 수가 일치한다면 break
+			if(N - i == target) break;
+		}
+		
+		//다른 그룹을 모두 묶었는데 target이 0이 아니라면
+		while(target-- > 0) { //1개짜리 그룹을 append 해준다.
+			sb.append(count).append(" ");
+			count = 1;
+		}
 	}
 
 	private static boolean isValidGroup(int mid) {
+		int sum = 0;
 		int groupCount = 1;
 		
-		int left = -1; int right = 1;
-		while(left <= right && right < N) {
-			int sum = 0;
-			if(left > 0) {
-				sum = DP[right] - DP[left];
-			} else sum += DP[right];
+		for(int i = 0; i < beads.length; i++) {
+			sum += beads[i];
 			
-			if(sum >= mid) { 
-				left = right; //새 구간부터 다시 부분합을 구한다.
-				right = left + 1;
+			if(sum > mid) {
+				sum = beads[i]; //새로운 구간부터 다시 부분합을 구한다.
 				groupCount++;
-			} else {
-				right++;
 			}
 		}
 		
