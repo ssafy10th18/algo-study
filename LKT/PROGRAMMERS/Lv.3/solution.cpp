647a6f87b9335e68b20b1223af7e31840d5de773
lkt9899 <lkt9899@naver.com>
2023-09-18 15:10:53 +0900

    [PRO] 입국 심사 / Lv3 / 15분

---
+++ LKT/PROGRAMMERS/Lv.3/solution.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+vector<int> *vp;
+int cnt, N;
+long long answer;
+
+long long calc(long long mid) {
+    long long sum = 0;
+    for (auto it = vp->begin(); it != vp->end(); it++) {
+        sum += (mid / *it);
+        if (sum > N) break;
+    }
+
+    return sum;
+}
+
+void find(long long start, long long end) {
+    if (start == end) return;
+
+    long long mid = (start + end) >> 1;
+    long long sum = calc(mid);
+    if (sum < N) {
+        find(mid + 1, end);
+    } else {
+        answer = mid;
+        find(start, mid);
+    }
+}
+
+long long solution(int n, vector<int> times) {
+    cnt = times.size();
+    N = n;
+    vp = &times;
+
+    long long start = 1;
+    long long end = 1000000000 / cnt;
+    end *= n;
+
+    find(start, end);
+
+    return answer;
+}
