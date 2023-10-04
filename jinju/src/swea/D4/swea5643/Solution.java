2cd77934df60671bd050d058f08acbc1df2813ef
jinju9553 <jinju9553@gmail.com>
2023-10-04 17:56:59 +0900

    Feat : 11주차 문제풀이

---
+++ jinju/src/swea/D4/swea5643/Solution.java
@@ -0,0 +1,13 @@
+package swea.D4.swea5643;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	}
+
+}
