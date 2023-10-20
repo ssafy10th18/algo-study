feb60563968b1c2025310e88cec44bb3060fe7c9
jinju9553 <jinju9553@gmail.com>
2023-10-20 17:57:56 +0900

    Feat : 13주차 문제풀이

--- jinju/src/boj/gold/boj12100/Main.java
+++ jinju/src/boj/gold/boj12100/Main.java
@@ -3,6 +3,7 @@ package boj.gold.boj12100;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main { // 2048 (Easy)
@@ -11,11 +12,11 @@ public class Main { // 2048 (Easy)
 	private static final int DOWN = 1;
 	private static final int LEFT = 2;
 	private static final int UP = 3;
-	
+
 	private static int[] dr = { 0, 1, 0, -1 }; // 오른쪽, 아래, 왼쪽, 위쪽
 	private static int[] dc = { 1, 0, -1, 0 };
 	private static int[][] matrix;
-	
+
 	private static int N;
 	private static int maxDepth = 5;
 	private static int max = -1;
@@ -50,10 +51,18 @@ public class Main { // 2048 (Easy)
 			}
 			return;
 		}
+		
+		//depth가 끝에 다다르지 않아도 max를 구한다.
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (max < matrix[i][j])
+					max = matrix[i][j];
+			}
+		}
 
 		for (int i = 0; i < dr.length; i++) {
 			int[][] newMatrix = new int[N][N];
-			
+
 			copyMatrix(matrix, newMatrix);
 			sumBlocks(i, newMatrix);
 			moveBlocks(i, newMatrix);
@@ -92,7 +101,7 @@ public class Main { // 2048 (Easy)
 		} else if (dir == LEFT) {// 인덱스 0부터 오른쪽으로 증가하면서 탐색
 			for(int i = 0; i < N; i++) {
 				int j = 0;
-				while(j < N) {
+				while(j < N - 1) {
 					if(matrix[i][j - dc[dir]] == matrix[i][j]) {
 						matrix[i][j] = matrix[i][j] * 2;
 						matrix[i][j - dc[dir]] = 0;
@@ -103,7 +112,7 @@ public class Main { // 2048 (Easy)
 		} else if (dir == UP) { // 인덱스 0부터 아래쪽으로 증가하면서 탐색
 			for(int j = 0; j < N; j++) {
 				int i = 0;
-				while(i < N) {
+				while(i < N - 1) {
 					if(matrix[i - dr[dir]][j] == matrix[i][j]) {
 						matrix[i][j] = matrix[i][j] * 2;
 						matrix[i - dr[dir]][j] = 0;
@@ -115,32 +124,67 @@ public class Main { // 2048 (Easy)
 	}
 	
 	private static void moveBlocks(int dir, int[][] matrix) {
-		if (dir == RIGHT) { 
-
-		} else if (dir == DOWN) {
-			for (int col = 0; col < N; col++) {
-				int row = N - 1; 
-				int nr = -1;
+		Stack<Integer> stack = new Stack<>();
+		
+		if (dir == RIGHT) { //맨 왼쪽부터 push하고 맨 오른쪽부터 pop
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					if(matrix[i][j] == 0) continue;
+					stack.push(matrix[i][j]);
+					matrix[i][j] = 0;
+				}
 				
-				while(row > 0) {
-					if(matrix[row][col] == 0) { //빈칸이라면 내릴 벽돌을 찾는다.
-						nr = row - 1;
-						while(nr > 0 && matrix[nr][col] == 0) --nr; //행을 위로 올린다.
-						
-						matrix[row][col] = matrix[nr][col]; //새로 찾은 벽돌을 빈 칸에 넣는다.
-						matrix[nr][col] = 0;
-					}
-					if(nr == 0) break; //해당 행의 탐색 종료
-					--row; //아직 해당 행을 다 못 봤다면 계속 탐색
+				for(int j = N - 1; j >= 0; j--) {
+					if(stack.isEmpty()) { break; }
+					
+					matrix[i][j] = stack.pop();
+				}
+			}
+		} else if (dir == DOWN) { //맨 윗쪽부터 push하고 맨 아래쪽부터 pop
+			for(int j = 0; j < N; j++) {
+				for(int i = 0; i < N; i++) {
+					if(matrix[i][j] == 0) continue;
+					stack.push(matrix[i][j]);
+					matrix[i][j] = 0;
+				}
+				
+				for(int i = N - 1; i >= 0; i--) {
+					if(stack.isEmpty()) { break; }
+					
+					matrix[i][j] = stack.pop();
 				}
 			}
 		} else if (dir == LEFT) {
-
+			for(int i = 0; i < N; i++) {				
+				for(int j = N - 1; j >= 0; j--) {
+					if(matrix[i][j] == 0) continue;
+					stack.push(matrix[i][j]);
+					matrix[i][j] = 0;
+				}
+				
+				for(int j = 0; j < N; j++) {
+					if(stack.isEmpty()) { break; }
+					
+					matrix[i][j] = stack.pop();
+				}
+			}
 		} else if (dir == UP) {
-			
+			for(int j = 0; j < N; j++) {
+				for(int i = N - 1; i >= 0; i--) {
+					if(matrix[i][j] == 0) continue;
+					stack.push(matrix[i][j]);
+					matrix[i][j] = 0;
+				}
+				
+				for(int i = 0; i < N; i++) {
+					if(stack.isEmpty()) { break; }
+					
+					matrix[i][j] = stack.pop();
+				}
+			}
 		}
 	}
-	
+
 	private static void copyMatrix(int[][] matrix, int[][] newMatrix) {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
@@ -152,7 +196,7 @@ public class Main { // 2048 (Easy)
 	private static boolean isSameMatrix(int[][] matrix, int[][] newMatrix) {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (matrix[i][j] == newMatrix[i][j])
+				if (matrix[i][j] != newMatrix[i][j])
 					return false;
 			}
 		}
